c6a61d36fec4a175e88b7acabaa1ccf115d37107
Charley Kevin <charleykevin@protonmail.ch>
2024-02-24 19:16:47 -0300

    accessing properties from javascript

--- resources/views/livewire/example.blade.php
+++ resources/views/livewire/example.blade.php
@@ -3,4 +3,12 @@
     <br>
     <x-text-input wire:model.live="name" />
     <x-primary-button wire:click="refresh">Refresh</x-primary-button>
+
+    <br><br>
+
+    <div x-data="">
+        <span x-text="$wire.name"></span>
+        <x-secondary-button @click="$wire.set('name', 'Alpine')">Set Alpine alter in backend</x-secondary-button>
+        <x-secondary-button @click="$wire.name= 'Alpine'">Wire Name alter in screen</x-secondary-button>
+    </div>
 </div>
