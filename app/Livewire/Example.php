93e4b8df705885ffd255e451c06ae7f82eb55e99
Charley Kevin <charleykevin@protonmail.ch>
2024-02-24 19:24:57 -0300

    lock property manipulation from client-side

--- app/Livewire/Example.php
+++ app/Livewire/Example.php
@@ -2,10 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Locked;
 use Livewire\Component;
 
 class Example extends Component
 {
+    #[Locked]
+    public int $id = 1;
+
     public string $name = 'Charley';
     public string $lastName = '';
 
@@ -18,6 +22,7 @@ class Example extends Component
     public function mount()
     {
         $this->fill([
+            'id' => 1,
             'name' => 'John',
             'lastName' => 'Doe'
         ]);
