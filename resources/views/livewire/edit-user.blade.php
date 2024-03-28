c1153ac505fa9bed1a0e7077c4e3a9874d3155f6
Charley Kevin <charleyk05@gmail.com>
2024-03-27 23:39:23 -0300

    Use wire:dirty

--- resources/views/livewire/edit-user.blade.php
+++ resources/views/livewire/edit-user.blade.php
@@ -1,7 +1,10 @@
 <div>
     <form wire:submit="submit" class="flex flex-col gap-4">
         <div>
-            <x-text-input placeholder="User Name" wire:model.live="form.name"/>
+            <x-text-input placeholder="User Name" wire:model.blur="form.name"/>
+            <div wire:dirty wire:target="form.name" class="text-sm italic font-semibold text-yellow-400">
+                Alteration detected, but not saved yet.
+            </div>
             @error('form.name')
             <div class="text-sm italic font-semibold text-red-400">{{ $message }}</div>
             @enderror
