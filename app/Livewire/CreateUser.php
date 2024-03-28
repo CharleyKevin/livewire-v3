828e419340d6b0911a8a2bfdf30a31694d99590d
Charley Kevin <charleyk05@gmail.com>
2024-03-27 23:33:09 -0300

    New livewire form

---
+++ app/Livewire/CreateUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Livewire\Forms\UserForm;
+use App\Models\User;
+use Livewire\Component;
+
+class CreateUser extends Component
+{
+    public UserForm $form;
+
+    public function render()
+    {
+        return view('livewire.create-user');
+    }
+
+    public function submit()
+    {
+        $this->form->save();
+    }
+}
---
+++ resources/views/livewire/create-user.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <form wire:submit="submit" class="flex flex-col gap-4">
+        <div>
+            <x-text-input placeholder="User Name" wire:model.live="form.name"/>
+            @error('form.name')
+            <div class="text-sm italic font-semibold text-red-400">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-text-input placeholder="Email" wire:model.live="form.email"/>
+            @error('form.email')
+            <div class="text-sm italic font-semibold text-red-400">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-text-input placeholder="Password" wire:model.live="form.password" type="password"/>
+            @error('form.password')
+            <div class="text-sm italic font-semibold text-red-400">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-text-input placeholder="Confirm Password" wire:model.live="form.password_confirmation" type="password"/>
+        </div>
+        <div>
+            <x-primary-button type="submit">Create User</x-primary-button>
+        </div>
+
+    </form>
+</div>
